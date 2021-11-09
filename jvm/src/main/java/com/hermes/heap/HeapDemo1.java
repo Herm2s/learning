da9e06af21844b3da0e3f46e3fb3a11914720a34
liuzongbin <lzb9599@qq.com>
2021-11-09 11:46:21 +0800

    feature: 文件提交

---
+++ jvm/src/main/java/com/hermes/heap/HeapDemo1.java
@@ -0,0 +1,19 @@
+package com.hermes.heap;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author liuzongbin
+ * @version 1.0
+ * @date 2021/9/18
+ */
+public class HeapDemo1 {
+
+    public static void main(String[] args) {
+        try {
+            TimeUnit.SECONDS.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ jvm/src/main/java/com/hermes/jdk/StreamDemo.java
@@ -0,0 +1,16 @@
+package com.hermes.jdk;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+
+/**
+ * @author liuzongbin
+ * @version 1.0
+ * @date 2021/10/12
+ */
+public class StreamDemo {
+
+
+    public static void main(String[] args) {
+
+    }
+}
