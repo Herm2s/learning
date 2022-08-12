1d2df7ac285d68d77383dcbf5bf365d607d0d9a7
hermes <1142305913@qq.com>
2022-08-12 18:52:22 +0800

    threads 1

--- algorithm/src/main/java/threads/Question1116.java
+++ algorithm/src/main/java/threads/Question1116.java
@@ -1,6 +1,5 @@
 package threads;
 
-import javax.print.attribute.standard.PrinterURI;
 import java.util.function.IntConsumer;
 
 /**
@@ -11,9 +10,7 @@ public class Question1116 {
 
     private int n;
 
-    private volatile int next = 0;
-
-    private volatile int isEven = 1;
+    private volatile int state;
 
     public Question1116(int n) {
         this.n = n;
@@ -22,28 +19,37 @@ public class Question1116 {
     // printNumber.accept(x) outputs "x", where x is an integer.
     public void zero(IntConsumer printNumber) throws InterruptedException {
         for (int i = 0; i < n; i++) {
-            synchronized (this) {
-                while (next != 0) {
-                    this.wait();
-                }
-                printNumber.accept(n);
-                next = 1;
+            while (state != 0) {
+                Thread.yield();
             }
-        }
-    }
-
-    public void even(IntConsumer printNumber) throws InterruptedException {
-        synchronized (this) {
-            while (isEven == 1) {
-                printNumber.accept(n);
-                next = 0;
+            printNumber.accept(0);
+            if (i % 2 == 0) {
+                state = 1;
+            } else {
+                state = 2;
             }
         }
     }
 
     public void odd(IntConsumer printNumber) throws InterruptedException {
-        printNumber.accept(n);
-        next = 0;
+        for (int i = 1; i <= n; i += 2) {
+            while (state != 1) {
+                Thread.yield();
+            }
+            printNumber.accept(i);
+            state = 0;
+        }
+
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i += 2) {
+            while (state != 2) {
+                Thread.yield();
+            }
+            printNumber.accept(i);
+            state = 0;
+        }
     }
 
     public static void main(String[] args) {
@@ -58,17 +64,18 @@ public class Question1116 {
 
         new Thread(() -> {
             try {
-                question.even(System.out::println);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }, "even").start();
-        new Thread(() -> {
-            try {
                 question.odd(System.out::println);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }, "odd").start();
+
+        new Thread(() -> {
+            try {
+                question.even(System.out::println);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, "even").start();
     }
 }
