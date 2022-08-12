f30c4084236c06ad247e5a4654bfd3abd63b8cc3
hermes <1142305913@qq.com>
2022-08-12 18:56:59 +0800

    h2o

---
+++ algorithm/src/main/java/threads/Question1117.java
@@ -0,0 +1,26 @@
+package threads;
+
+/**
+ * leetcode题号1117
+ *
+ * @author liu.zongbin
+ * @since 2022/8/12
+ */
+public class Question1117 {
+
+    public Question1117() {
+
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        releaseHydrogen.run();
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+        releaseOxygen.run();
+    }
+}
