3514a1dba80567d048d9783dd4b87ef8ff136ca7
herm2s <1142305913@qq.com>
2022-08-25 22:43:51 +0800

    算法

--- algorithm/src/main/java/com/hermes/algorithm/string/Question151.java
+++ algorithm/src/main/java/com/hermes/algorithm/string/Question151.java
@@ -7,7 +7,64 @@ package com.hermes.algorithm.string;
 public class Question151 {
 
     public static String reverseWords(String s) {
-        return s;
+        // 去除首尾及中间多余空格
+        StringBuilder sb = removeSpace(s);
+        // 反转整个字符串
+        reverse(sb, 0, sb.length() - 1);
+        // 反转各个单词
+        reverseEachWord(sb);
+        return sb.toString();
+    }
+
+    private static void reverseEachWord(StringBuilder sb) {
+        int left = 0;
+        int right = 1;
+        int n = sb.length();
+        while (left < n) {
+            while (right < n && sb.charAt(right) != ' ') {
+                right++;
+            }
+            reverse(sb, left, right- 1);
+            left = right + 1;
+            right = left + 1;
+        }
+    }
+
+    private static StringBuilder reverse(StringBuilder sb, int left, int right) {
+        for (int i = left, j = right; i < j; i++, j--) {
+            char temp = sb.charAt(i);
+            sb.setCharAt(i, sb.charAt(j));
+            sb.setCharAt(j, temp);
+        }
+        return sb;
+    }
+
+    private static StringBuilder removeSpace(String s) {
+        int slow = 0;
+        int fast = 0;
+        // 去掉字符串前面的冗余空格
+        while (s.length() > 0 && fast < s.length() && s.charAt(fast) == ' ') {
+            fast++;
+        }
+        StringBuilder sb = new StringBuilder(s);
+        for (; fast < sb.length(); fast++) {
+            // 去掉字符串中间部分的冗余空格
+            if (fast - 1 > 0
+                    && sb.charAt(fast) == ' '
+                    && sb.charAt(fast - 1) == sb.charAt(fast)) {
+                continue;
+            } else {
+                sb.setCharAt(slow++, sb.charAt(fast));
+            }
+        }
+        // 去掉字符串末尾空格
+        StringBuilder result;
+        if (slow - 1 > 0 && sb.charAt(slow - 1) == ' ') {
+            result = new StringBuilder(sb.substring(0, slow - 1));
+        } else {
+            result = new StringBuilder(sb.substring(0, slow));
+        }
+        return result;
     }
 
     public static void main(String[] args) {
