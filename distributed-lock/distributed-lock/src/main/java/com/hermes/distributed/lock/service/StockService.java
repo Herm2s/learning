6a6a9b6ec2ae2289a13bb950948c953e3db2c36f
herm2s <1142305913@qq.com>
2022-08-30 22:04:58 +0800

    扣减库存加JVM锁

--- distributed-lock/distributed-lock/src/main/java/com/hermes/distributed/lock/service/StockService.java
+++ distributed-lock/distributed-lock/src/main/java/com/hermes/distributed/lock/service/StockService.java
@@ -3,6 +3,8 @@ package com.hermes.distributed.lock.service;
 import com.hermes.distributed.lock.pojo.Stock;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author liu.zongbin
  * @since 2022/8/30 21:49
@@ -12,8 +14,15 @@ public class StockService {
 
     private Stock stock = new Stock();
 
+    private ReentrantLock lock = new ReentrantLock();
+
     public void deduct() {
-        stock.setStock(stock.getStock() - 1);
+        try {
+            this.lock.lock();
+            stock.setStock(stock.getStock() - 1);
+        } finally {
+            this.lock.unlock();
+        }
         System.out.println("库存余量：" + this.stock.getStock());
     }
 }
