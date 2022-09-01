84845dcee5b34a5bd2fdcbc729a7dcf68aa6b3ac
herm2s <1142305913@qq.com>
2022-09-01 22:40:10 +0800

    redis分布式锁

--- distributed-lock/src/main/java/com/hermes/distributed/lock/service/StockService.java
+++ distributed-lock/src/main/java/com/hermes/distributed/lock/service/StockService.java
@@ -1,11 +1,17 @@
 package com.hermes.distributed.lock.service;
 
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.hermes.distributed.lock.mapper.StockMapper;
-import com.hermes.distributed.lock.pojo.Stock;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -18,15 +24,73 @@ public class StockService {
 
     private final StockMapper stockMapper;
 
-    private ReentrantLock lock = new ReentrantLock();
+    private final StringRedisTemplate redisTemplate;
+
+    private ReentrantLock reentrantLock = new ReentrantLock();
 
     public void deduct() {
-        try {
-            Stock stock = this.stockMapper.selectOne(Wrappers.lambdaQuery(Stock.class)
-                    .eq(Stock::getProductCode, "1001"));
-            this.lock.lock();
-        } finally {
-            this.lock.unlock();
+        // 循环重试加锁
+        while (!this.redisTemplate.opsForValue().setIfAbsent("lock", "111")) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(50);
+                this.deduct();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+        try {
+            // 1.查询库存信息
+            String stock = redisTemplate.opsForValue().get("stock");
+            // 2.判断库存是否充足
+            if (stock != null && stock.length() > 0) {
+                int st = Integer.parseInt(stock);
+                if (st > 0) {
+                    // 3.扣减库存
+                    redisTemplate.opsForValue().set("stock", String.valueOf(--st));
+                }
+            }
+        } finally {
+            // 解锁
+            this.redisTemplate.delete("lock");
+        }
+    }
+
+
+    /**
+     * redis事务锁库存
+     */
+    public void deduct4() {
+        this.redisTemplate.execute(new SessionCallback<Object>() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                // watch
+                operations.watch("stock");
+                // 1.查询库存信息
+                String stock = Objects.requireNonNull(operations.opsForValue().get("stock")).toString();
+
+                // 2.判断库存是否充足
+                if (stock != null && stock.length() > 0) {
+                    Integer st = Integer.valueOf(stock);
+                    if (st > 0) {
+                        // multi
+                        operations.multi();
+                        operations.opsForValue().set("stock", String.valueOf(--st));
+                        // exec
+                        List exec = operations.exec();
+                        if (CollectionUtils.isEmpty(exec)) {
+                            // 重试
+                            try {
+                                TimeUnit.MILLISECONDS.sleep(40);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            deduct();
+                        }
+                        return exec;
+                    }
+                }
+                return null;
+            }
+        });
     }
 }
