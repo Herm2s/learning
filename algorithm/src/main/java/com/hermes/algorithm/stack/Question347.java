64bb4b3eba3d4ec21432dda9ef1f8f2c1a290937
herm2s <1142305913@qq.com>
2022-09-05 21:07:29 +0800

    小顶堆运用

--- algorithm/src/main/java/com/hermes/algorithm/stack/Question347.java
+++ algorithm/src/main/java/com/hermes/algorithm/stack/Question347.java
@@ -1,6 +1,6 @@
 package com.hermes.algorithm.stack;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * @author liu.zongbin
@@ -12,7 +12,31 @@ public class Question347 {
         if (nums.length == 1) {
             return nums;
         }
-        return nums;
+        // key: num，value: count
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.merge(num, 1, Integer::sum);
+        }
+        // 小顶堆，int[] 的第一个元素就是num，第二个元素就是count
+        PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(n -> n[1]));
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int num = entry.getKey();
+            int count = entry.getValue();
+            if (queue.size() == k) {
+                if (queue.peek()[1] < count) {
+                    // 移除根节点
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            } else {
+                queue.offer(new int[]{num, count});
+            }
+        }
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = queue.poll()[0];
+        }
+        return result;
     }
 
     public static void main(String[] args) {
