fd194a6428a2f8fb467c7af5388703c959e42580
herm2s <1142305913@qq.com>
2022-09-13 22:00:49 +0800

    算法

---
+++ algorithm/src/main/java/com/hermes/algorithm/binarytree/Question404.java
@@ -0,0 +1,42 @@
+package com.hermes.algorithm.binarytree;
+
+/**
+ * @author liu.zongbin
+ * @since 2022/9/13 20:16
+ */
+public class Question404 {
+
+    public int sumOfLeftLeaves(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        if (node.left == null && node.right == null) {
+            return 0;
+        }
+        int leftNum = sumOfLeftLeaves(node.left);
+        if (node.left != null && node.left.left == null && node.left.right == null) {
+            leftNum = node.left.val;
+        }
+        int rightNum = sumOfLeftLeaves(node.right);
+        return leftNum + rightNum;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
