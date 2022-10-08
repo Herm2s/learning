f4698c3673a26f6d294959511a60397a0e3855ee
hermes <1142305913@qq.com>
2022-10-08 17:27:30 +0800

    算法

---
+++ algorithm/src/main/java/com/hermes/algorithm/binarytree/Question236.java
@@ -0,0 +1,48 @@
+package com.hermes.algorithm.binarytree;
+
+/**
+ * @author liu.zongbin
+ * @since 2022/10/8
+ */
+public class Question236 {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // 递归结束条件
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        // 后序遍历
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        // 未找到节点p或者节点q
+        if (left == null && right == null) {
+            return null;
+        }
+        // 若找到一个节点
+        else if (left == null && right != null) {
+            return right;
+        } else if (left != null && right == null) {
+            return left;
+        }
+        // 若找到两个节点
+        else {
+            return root;
+        }
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
