6e45fbec8b7bf52bf953962916961f4313631d36
herm2s <1142305913@qq.com>
2022-10-09 14:14:41 +0800

    算法

---
+++ algorithm/src/main/java/com/hermes/algorithm/binarytree/Question235.java
@@ -0,0 +1,40 @@
+package com.hermes.algorithm.binarytree;
+
+/**
+ * @author liu.zongbin
+ * @since 2022/10/9
+ */
+public class Question235 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        // p和q在左子树
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        // p和q在右子树
+        else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        // 找到最近祖先
+        else {
+            return root;
+        }
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
