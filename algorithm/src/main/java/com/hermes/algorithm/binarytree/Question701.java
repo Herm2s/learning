67b6d551538e799a5545e6a0783f3f37339eb3da
hermes <1142305913@qq.com>
2022-10-10 18:31:49 +0800

    算法

---
+++ algorithm/src/main/java/com/hermes/algorithm/binarytree/Question701.java
@@ -0,0 +1,54 @@
+package com.hermes.algorithm.binarytree;
+
+/**
+ * @author liu.zongbin
+ * @since 2022/10/10
+ */
+public class Question701 {
+
+    public static void main(String[] args) {
+        Question701 question701 = new Question701();
+        TreeNode root = new TreeNode(4);
+        TreeNode a = new TreeNode(2);
+        TreeNode b = new TreeNode(7);
+        TreeNode c = new TreeNode(1);
+        TreeNode d = new TreeNode(3);
+
+        root.left = a;
+        root.right = b;
+        a.left = c;
+        a.right = d;
+        System.out.println(question701.insertIntoBST(root, 5));
+    }
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        } else if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
