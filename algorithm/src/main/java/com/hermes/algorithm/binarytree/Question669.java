cc5dd12817edb58505b2723a7d9ccc13911bb0c4
herm2s <1142305913@qq.com>
2022-10-12 20:49:47 +0800

    算法

--- algorithm/src/main/java/com/hermes/algorithm/binarytree/Question669.java
+++ algorithm/src/main/java/com/hermes/algorithm/binarytree/Question669.java
@@ -9,7 +9,18 @@ package com.hermes.algorithm.binarytree;
 public class Question669 {
 
     public TreeNode trimBST(TreeNode root, int low, int high) {
-        return null;
+        if (root == null) {
+            return root;
+        }
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        }
+        if (root.val > high) {
+            return trimBST(root.left, low, high);
+        }
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
     }
 
     public static void main(String[] args) {
