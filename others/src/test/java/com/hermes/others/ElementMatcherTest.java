ce2f65c3f2a6fee862045215d3d4e791350d449b
hermes <1142305913@qq.com>
2023-02-13 10:48:11 +0800

    feat: 后台订单优化

--- others/src/test/java/com/hermes/others/ElementMatcherTest.java
+++ others/src/test/java/com/hermes/others/ElementMatcherTest.java
@@ -1,5 +1,8 @@
 package com.hermes.others;
 
+import net.bytebuddy.matcher.ElementMatcher;
+import net.bytebuddy.matcher.ElementMatchers;
+import net.bytebuddy.matcher.StringMatcher;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -10,5 +13,22 @@ class ElementMatcherTest {
 
     @Test
     void test1() {
+        System.out.println(new StringMatcher("va", StringMatcher.Mode.CONTAINS)
+                .matches("value"));
+    }
+
+    public static ElementMatcher.Junction<String> containsA() {
+        return new StringMatcher("a", StringMatcher.Mode.CONTAINS);
+    }
+
+    public static ElementMatcher.Junction<String> containsB() {
+        return new StringMatcher("b", StringMatcher.Mode.CONTAINS);
+    }
+
+    public static void main(String[] args) {
+        ElementMatcher.Junction<Object> objectJunction = ElementMatchers.anyOf("");
+        System.out.println(containsA()
+                .and(containsB())
+                .matches("ab"));
     }
 }
